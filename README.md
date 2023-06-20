2b07bfdab618575122742c1a1a7a9a6c7b5d5132
JayBassCodes <jacob.m.bassett@gmail.com>
2023-06-19 21:07:45 -0400

    added generator code as well as filled out README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password-Generator
+
+## JavaScript powered password generation
+
+This website it to be used to generate a password of 8-128 characters. The password may include uppercase, lowercase, numbers and/or special characters. Just press the "Generate Password" button and answer the prompts to get a unique password generated.
+
+## Website on landing
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/1dfdbbd3-4fd5-4b93-917c-e71f0fcfd975)
+
+## Various prompts
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/cd4a991b-513f-4a81-a933-e02a6fabc811)
+
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/26221b71-ec73-4359-9a7e-9db46e89dee2)
+
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/918599a4-c6b3-49ba-92f3-e0224792a56b)
+
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/7afb222e-2a3a-469c-9919-47012a75f274)
+
+![image](https://github.com/JayBassCodes/Jacob-Bassett-Portfolio/assets/132828489/053b21b1-e4a8-4ddf-ab03-d359bdf53e05)
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
+console.log(window)
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = "!@#$%^&*()";
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +14,73 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  // Variable Block
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
+  var upperCases = confirm("Do you want uppercases in your password?");
+  var special = confirm("Do you want special characters in your password?");
+  var minimumCharacters  = 0;
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+
+  // 
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10));
+    },
+
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26));
+    },
+
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26));
+    },
+
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+  };
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCharacters++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCharacters++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCharacters++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCharacters++;
+
+  }
+  var randomPasswordGenerated = "";
+
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCharacters); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+    randomPasswordGenerated += randomNumberPicked;
+  }
+
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+
+  return randomPasswordGenerated;
+}
